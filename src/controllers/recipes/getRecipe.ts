44c03b4bf1bbaafe2d6e20ce74ff4b0d540eece8
eric tang <ericjiantang@cnspartner.com>
2026-01-15 23:39:43 -0600

    trigger

--- src/controllers/recipes/getRecipe.ts
+++ src/controllers/recipes/getRecipe.ts
@@ -26,7 +26,7 @@ export async function getRecipe(req, res) {
           "1/4 cup parsley",
           "8 oz fettuccine pasta",
         ],
-        cookingTime: 20,
+        cookingTime: 30,
         instructions: [
           "Cook pasta according to package instructions",
           "Season chicken with salt, pepper, Italian seasoning, and paprika",
